e91b60e698baf80747f3bfb96f5b0f83baa008d9
keteb <ketebmehdi2@gmail.com>
2023-03-05 16:23:44 +0100

    init

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,5 +1,7 @@
 from graph import Graph, graph_from_file, get_path_with_power, get_path_with_power_dist, min_power
 
-filename = "network.01.in"
+filename = "network.test.in"
+
 g = graph_from_file(filename)
 
+print(g)
