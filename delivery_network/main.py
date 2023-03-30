c2047f33f9abbd76272a3b285ef5b24b2b275a40
keteb <ketebmehdi2@gmail.com>
2023-03-30 11:50:09 +0200

    truck1

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -38,3 +38,32 @@ print(process(ot))
 
 
 # %%
+
+def trucks_filter(filename_trucks):
+    with open (filename_trucks, 'r') as file:
+        nb_trucks = int(file.readline())
+        trucks = []
+        for _ in range(nb_trucks):
+            power, cost = file.readline().split()
+            trucks.append((int(power), int(cost)))
+    file.close()
+
+    trucks.sort(key = lambda x: (x[0], -x[1]))
+    trucks_filter = [trucks[-1]]
+    for elt in trucks[-2::-1]:
+        if elt[1] < trucks_filter[-1][1]:
+            trucks_filter.append(elt)
+    return trucks_filter[::-1]
+
+filename_trucks = r'C:\Users\keteb\OneDrive\Bureau\Projet_graph_py\input\trucks.0.in'
+
+print(trucks_filter(filename_trucks))
+
+
+
+
+
+
+
+
+# %%
